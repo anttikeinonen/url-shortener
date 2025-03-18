6d4be10d5d6de10b614aa2865fbc508d255b8abc
anttikeinonen <52934450+anttikeinonen@users.noreply.github.com>
2025-03-18 12:23:48 +0200

    implemented basic logic

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,52 @@
 import { Router } from "express";
+import Url from "./models/url";
+
+async function generateUniqueShortUrl(): Promise<string> {
+    let shortUrl;
+    let exists;
+
+    do {
+        shortUrl = Math.random().toString(36).substring(2, 6);
+        exists = await Url.findOne({ where: { shortUrl } });
+    } while (exists !== null);
+
+    return shortUrl;
+}
 
 const router = Router();
 
-router.post("/shorten", async (req, res) => {
+router.post("/", async (req, res) => {
     const { url } = req.body;
-    const shortUrl = "http://localhost:3000/short/1234";
+    if (!url) {
+        res.status(400).json({ error: "URL is required" });
+        return;
+    }
+    const shortUrl = await generateUniqueShortUrl();
+    await Url.create({ originalUrl: url, shortUrl });
     res.json({ shortUrl });
 });
 
-router.get("/short/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
     const { id } = req.params;
-    res.redirect("https://www.google.com");
+    const url = await Url.findOne({ where: { shortUrl: id } });
+    if (url === null) {
+        res.status(404).json({ error: "Short URL not found" });
+    } else {
+        url.hits += 1;
+        await url.save();
+        res.redirect(url.originalUrl);
+    }
 });
 
-router.get("/short/:id/stats", async (req, res) => {
+router.get("/:id/stats", async (req, res) => {
     const { id } = req.params;
-    res.json({ id, hits: 100 });
+    const url = await Url.findOne({ where: { shortUrl: id } });
+    if (url === null) {
+        res.status(404).json({ error: "Short URL not found" });
+    } else {
+        const { id: _, ...data } = url.dataValues;
+        res.json(data);
+    }
 });
 
 export default router;
