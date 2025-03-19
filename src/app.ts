b4291724bff965bf216e9a7276f245c94a8ca871
anttikeinonen <52934450+anttikeinonen@users.noreply.github.com>
2025-03-19 11:37:15 +0200

    database init fix

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import routers from "./router";
 import helmet from "helmet";
+import sequelize from "./sequelize";
 
 const app = express();
 
@@ -10,6 +11,12 @@ app.use(cors({
     origin: "*"
 }));
 app.use(express.json());
+
+app.use(async (_req, _res, next) => {
+    await sequelize.sync();
+    next();
+})
+
 app.use(routers);
 
 export default app
--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,4 +1,7 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL is not defined');
@@ -13,5 +16,3 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 });
 
 export default sequelize;
-
-sequelize.sync();
